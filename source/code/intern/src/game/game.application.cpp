f1994109593fdb5e0a3c95b8038d8b525690d1b3
Robert Ackermann <robert_ackermann@icloud.com>
2023-04-24 09:25:57 +0200

    fixed project settings for all projects

--- source/code/intern/src/game/game.application.cpp
+++ source/code/intern/src/game/game.application.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::CircleShape shape(200.f);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
