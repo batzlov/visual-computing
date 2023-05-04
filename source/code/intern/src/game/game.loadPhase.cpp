575855bfbf01491753c2cd76fb1a0678ff8cba5d
Robert Ackermann <robert_ackermann@icloud.com>
2023-05-04 13:18:18 +0200

    fixed errors in experimental level

--- source/code/intern/src/game/game.loadPhase.cpp
+++ source/code/intern/src/game/game.loadPhase.cpp
@@ -27,7 +27,8 @@ namespace Game
     {
 
         XMLDocument* document = new XMLDocument();
-        std::string filePath = Core::Config::levelsDir + "level-1.xml";
+        // std::string filePath = Core::Config::levelsDir + "level-1.xml";
+        std::string filePath = Core::Config::levelsDir + "level-experimental.xml";
         document->LoadFile(filePath.c_str());
 
         Data::LoadPhase::GetInstance().OnRun(*document);
